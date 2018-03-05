6922e3c5df1b3d55098e9c9a2d6c8cfe16455a27
Marlos Pomin <marlospomin@gmail.com>
2018-03-05 14:53:56 -0300

    Add test structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,17 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const rename = require("gulp-rename");
+const karma = require('karma').Server;
 const uglify = require('gulp-uglifyjs');
-const browserSync = require('browser-sync');
 
-// Browser-sync task
-gulp.task('server', function() {
-  browserSync.init({
-    server: "./example/"
-  });
-
-  // Watch tasks
-  gulp.watch(['./src/*.js'], ['reload']);
-  gulp.watch(['./example/*.html', './example/js/*.js', './example/css/*.css'],
-    browserSync.reload);
-});
-
-// Reload task
-gulp.task('reload', function() {
-  gulp.src(['./src/mango.js'])
-    .pipe(babel({
-      presets: ['env'],
-      plugins: ['add-module-exports', 'transform-object-rest-spread',
-        'transform-es2015-modules-umd'
-      ]
-    }))
-    .pipe(uglify())
-    .pipe(rename('mango.min.js'))
-    .pipe(gulp.dest('./example/js'))
+// Test task
+gulp.task('test', function (done) {
+  new karma({
+    configFile: __dirname + '/karma.conf.js'
+  }, function (exitCode) {
+    done();
+    process.exit(exitCode);
+  }).start();
 });
 
 // Build task
@@ -46,4 +30,4 @@ gulp.task('build', function() {
 });
 
 // Set the defaukt task as server
-gulp.task('default', ['server']);
+gulp.task('default', ['build']);
