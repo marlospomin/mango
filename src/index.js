acae03eec1d98f3868ffd803a002094b33a10e97
Marlos Pomin <marlospomin@gmail.com>
2020-05-06 16:31:34 -0300

    Update entry file

--- src/index.js
+++ src/index.js
@@ -3,76 +3,58 @@ const defaultConfig = {
   selector: '[data-mango]',
   // Default background color
   background: 'white',
-  // Default margin
+  // Default margin in px
   margin: 50,
   // Default cancel keys
-  interruptKeys: [27, 37, 39]
+  interruptKeys: [27, 37, 39],
 }
 
-export default function (config = {}) {
+export default function(config = {}) {
   // Push default into config
   const { selector, background,
-     margin, interruptKeys } = { ...defaultConfig, ...config }
-
+    margin, interruptKeys } = { ...defaultConfig, ...config }
   // Load each image into images
-  const images = select()
-  // Save the created wrapper into the var
-  const wrapper = wrap()
+  const images = document.querySelectorAll(selector)
   // Create a control var
   let isAnimating = false
 
-  function select() {
-    // Load all the images given selector
-    const images = document.querySelectorAll(selector)
-    // Return image collection
-    return images
-  }
-
-  function clone(element) {
+  const clone = (element) => {
     // Save values into vars
     const { top, left, width, height } = element.getBoundingClientRect()
     // Clone the element
-    const clone = element.cloneNode()
+    const copy = element.cloneNode()
     // Save the scrolled amounts into vars
     const scrollTop = window.pageYOffset || 0
     const scrollLeft = window.pageXOffset || 0
     // Set custom style for the fake element
-    clone.style.position = 'absolute'
-    clone.style.top = `${ top + scrollTop }px`
-    clone.style.left = `${ left + scrollLeft }px`
-    clone.style.width = `${ width }px`
-    clone.style.height = `${ height }px`
+    copy.style.position = 'absolute'
+    copy.style.top = `${ top + scrollTop }px`
+    copy.style.left = `${ left + scrollLeft }px`
+    copy.style.width = `${ width }px`
+    copy.style.height = `${ height }px`
     // Return the cloned element
-    return clone
+    return copy
   }
 
-  function wrap() {
+  const wrap = (backgroundColor) => {
     // Create an overlay
-    const wrapper = document.createElement('div')
+    const cover = document.createElement('div')
     // Style the overlay
-    wrapper.classList.add('mango-overlay')
+    cover.classList.add('mango-overlay')
     // Style the background color
-    wrapper.style.backgroundColor = background
+    cover.style.backgroundColor = backgroundColor
     // Return the created overlay
-    return wrapper
+    return cover
   }
 
-  function trigger(event) {
+  const trigger = (event) => {
     // Prevent default
     event.preventDefault()
-    // If whoever triggered the event is an actual element
-    if (event.target) {
-      // Save the element who did that
-      const origin = event.target
-      // Zoom in
-      zoom(origin)
-    } else {
-      // Zoom out
-      zoom(origin).out(150)
-    }
+    // If whatever triggered the event is an actual element
+    event.target ? zoom(event.target) : zoom(event.target).out(150)
   }
 
-  function zoom(origin) {
+  const zoom = (origin) => {
     // If origin is not found break
     if (!origin) return
     // Set control var
@@ -81,20 +63,18 @@ export default function (config = {}) {
     const scrollTop = window.pageYOffset || 0
     // Save zoomed image into var
     const zoomed = clone(origin)
-    // Apply the overlay
-    document.body.appendChild(wrapper)
-    // Apply the clone
-    document.body.appendChild(zoomed)
+    // Save the created wrapper into the var
+    const wrapper = wrap(background)
+    // Apply elements to the DOM
+    document.body.append(wrapper, zoomed)
     // Request animation event
-    requestAnimationFrame(() => {
-      document.body.classList.add('mango--open')
-    })
+    requestAnimationFrame(() => document.body.classList.add('mango--open'))
     // Set style to the original image
     origin.style.visibility = 'hidden'
     // Set styles for the zoomed image
     zoomed.classList.add('mango-image--open')
     // Remove handler
-    let remove = function remove() {
+    let remove = () => {
       // Add events
       zoomed.addEventListener('click', () => {
         isAnimating = false
@@ -112,14 +92,14 @@ export default function (config = {}) {
       zoomed.removeEventListener('transitionend', remove)
     }
     // Keydown handler
-    const keydown = function remove() {
+    const keydown = () => {
       if (interruptKeys.includes(event.keyCode)) {
         isAnimating = false
         out()
       }
     }
     // Scroll handler
-    const scroll = function remove() {
+    const scroll = () => {
       // Save current scroll
       const currentScroll = window.pageYOffset || 0
       // If scrolled more than 50px zoom out
@@ -135,7 +115,7 @@ export default function (config = {}) {
     // Calculte and set transform
     if (origin.dataset.src) {
       // Fetchs an image
-      function fetchImage(url) {
+      const fetchImage = (url) => {
         return new Promise((resolve, reject) => {
           // Create a new image
           const image = new Image()
@@ -147,7 +127,7 @@ export default function (config = {}) {
         })
       }
       // Preloads an image
-      function preloadImage(image) {
+      const preloadImage = (image) => {
         // Save the src
         const src = image.dataset.src
         // Return to those 2 methods or fallback
@@ -155,76 +135,79 @@ export default function (config = {}) {
           () => zoomed.style.transform = calculate(origin))
       }
       // Apply the transform
-      function applyTransform(image, src) {
+      const applyTransform = (image, src) => {
         // Load src into the image
         image.src = src
         // Apply the real transform
-        const interval = setInterval(() => { clearInterval(interval)
-          image.style.transform = calculate(image) }, 20)
+        const interval = setInterval(() => {
+          clearInterval(interval)
+          image.style.transform = calculate(image)
+        }, 20)
       }
       // Run the preload
       preloadImage(zoomed)
-    } else {
+    }
+    else {
       // Fallback
       zoomed.style.transform = calculate(origin)
     }
-    // Zoom out function
-    function out(timeout = 0) {
+
+    const run = () => {
+      // If we are animating break
+      if (isAnimating) return
+      // Set control var
+      isAnimating = true
+      // Remove class from body
+      document.body.classList.remove('mango--open')
+      // De-transform the element
+      zoomed.style.transform = 'none'
+      // Remove event listener from click
+      zoomed.removeEventListener('click', out)
+      // Remove handler
+      remove = () => {
+        // Set visibility of the original image
+        origin.style.visibility = 'visible'
+        // Remove the cloned element and the wrapper
+        zoomed.remove()
+        wrapper.remove()
+        // Remove classes
+        zoomed.classList.remove('mango-image--open')
+        // Set control var
+        isAnimating = false
+        // Remove event itself
+        zoomed.removeEventListener('transitionend', remove)
+      }
+      // Add animations to zoom out
+      zoomed.addEventListener('transitionend', remove)
+      // Remove events
+      document.removeEventListener('keydown', keydown)
+      document.removeEventListener('scroll', scroll)
+    }
+    const out = (timeout = 0) => {
       // If timeout is more than 0 time it out else zoom out
       if (timeout > 0) {
         setTimeout(run, timeout)
-      } else {
-        run()
       }
-
-      function run() {
-        // If we are animating break
-        if (isAnimating) return
-        // Set control var
-        isAnimating = true
-        // Remove class from body
-        document.body.classList.remove('mango--open')
-        // De-transform the element
-        zoomed.style.transform = 'none'
-        // Remove event listener from click
-        zoomed.removeEventListener('click', out())
-        // Remove handler
-        remove = function remove() {
-          // Set visibility of the original image
-          origin.style.visibility = 'visible'
-          // Remove the cloned element and the wrapper
-          zoomed.remove()
-          wrapper.remove()
-          // Remove classes
-          zoomed.classList.remove('mango-image--open')
-          // Set control var
-          isAnimating = false
-          // Remove event itself
-          zoomed.removeEventListener('transitionend', remove)
-        }
-        // Add animations to zoom out
-        zoomed.addEventListener('transitionend', remove)
-        // Remove events
-        document.removeEventListener('keydown', keydown)
-        document.removeEventListener('scroll', scroll)
+      else {
+        run()
       }
     }
   }
 
-  function calculate(origin) {
+  const calculate = (origin) => {
     // Create a container
     const container = {
       width: window.innerWidth, height: window.innerHeight,
-      left: 0, top: 0, right: 0, bottom: 0
+      left: 0, top: 0, right: 0, bottom: 0,
     }
     // Set viewport vars
-    let viewportWidth = container.width - margin * 2
-    let viewportHeight = container.height - margin * 2
+    const viewportWidth = container.width - margin * 2
+    const viewportHeight = container.height - margin * 2
     // Set the zoom target
     const zoomTarget = origin
     // Save computed information
     const { naturalWidth = viewportWidth,
-       naturalHeight = viewportHeight } = zoomTarget
+      naturalHeight = viewportHeight } = zoomTarget
     const { top, left, width, height } = zoomTarget.getBoundingClientRect()
     // Get scale
     const scaleX = Math.min(naturalWidth, viewportWidth) / width
@@ -242,15 +225,13 @@ export default function (config = {}) {
     return transform
   }
 
-  function run(images) {
+  const run = (entries) => {
     // For each image in the collection run mango upon click
-    Array.from(images).forEach(image => {
+    Array.from(entries).forEach((entry) => {
       // Add styles to each image
-      image.classList.add('mango-image')
+      entry.classList.add('mango-image')
       // Trigger the zoom effect
-      image.addEventListener('click', (event) => {
-        trigger(event)
-      })
+      entry.addEventListener('click', (event) => trigger(event))
     })
   }
 
